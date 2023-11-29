ce523fbb7adc1bf3654275f18d28a9afdb5d2cf0
arturateo <arti.shishka11@gmail.com>
2023-11-29 19:52:53 +0600

    Day-1 made changes for 'Reverse Vowels'

--- reverse_vowels.py
+++ reverse_vowels.py
@@ -25,23 +25,47 @@
 # Output: "DusUgnGires"
 # ```
 
+# def change_vowels(word):
+#     data = ['a', 'e', 'i', 'o', 'u']
+#     new_word = list(map(lambda x: x, word))
+#     left = 0
+#     right = len(word) - 1
+#     while left < right:
+#         if word[left].lower() in data and word[right].lower() in data:
+#             new_word.insert(left, new_word.pop(right))
+#             new_word.insert(right, new_word.pop(left + 1))
+#             left += 1
+#             right -= 1
+#         elif not (word[left] in data):
+#             left += 1
+#         else:
+#             right -= 1
+#
+#     return "".join(new_word)
+
+
 def change_vowels(word):
     data = ['a', 'e', 'i', 'o', 'u']
-    new_word = list(map(lambda x: x, word))
+    left_word = ''
+    right_word = ''
     left = 0
     right = len(word) - 1
     while left < right:
         if word[left].lower() in data and word[right].lower() in data:
-            new_word.insert(left, new_word.pop(right))
-            new_word.insert(right, new_word.pop(left + 1))
+            left_word += word[left]
+            right_word += word[right]
             left += 1
             right -= 1
         elif not (word[left] in data):
+            left_word += word[left]
             left += 1
         else:
+            right_word += word[right]
             right -= 1
-
-    return "".join(new_word)
+    if len(left_word + right_word) != len(word):
+        left_word += word[left]
+    new_word = left_word + right_word[::-1]
+    return new_word
 
 
 print(change_vowels("hello"))
